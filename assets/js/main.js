e879666ecc95f5626f0e990e3363a6c8fef1f052
Naiem-Devs <dnsnaiem@gmail.com>
2022-12-27 13:35:27 +0600

    HVO-fixed v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,38 +6,13 @@
     $('.mobile-menu').toggleClass('siteBar');   
   }); 
 
-
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+        $('.header-area').addClass('sticky')
+    } else{
+        $('.header-area').removeClass('sticky')
     }
-  });
-
-  /* magnificPopup img view */
-  $(".popup-image").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true
-    }
-  }); 
+});
 
  
 })(jQuery);
